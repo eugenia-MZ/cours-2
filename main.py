9de089b35ed6188a9271f7051af7097edb3a0efc
Mehdi Zitouni <mzitouni@eugeniaschool.com>
2025-10-02 14:12:24 +0000

    début exercices

---
+++ main.py
@@ -0,0 +1,16 @@
+# TODO: Create a list of your favorite fruits
+fruits = ["pomme", "banane", "orange", "fraise", "kiwi"]
+
+# TODO: Add a new fruit to the end of the list
+fruits.append("poire")
+
+# TODO: Remove the second fruit from the list
+fruits.pop(1)
+
+# TODO: Sort the list alphabetically
+fruits.sort()
+# TODO: Create a new list with the first three fruits
+nouvelle_liste = fruits[0:3]
+print(f"Fruits :{fruits}")
+print(f"Nouvelle_liste :{nouvelle_liste}")
+# Print the original list and the new list
